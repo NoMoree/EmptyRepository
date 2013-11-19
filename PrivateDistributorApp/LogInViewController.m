e263f0e2953fd041020c95d01e6fde45da3c648c
VindicatoIOS <vindicatoios@VindicatoIOSs-Mac.local>
2013-11-20 01:42:12 +0200

    init

---
+++ PrivateDistributorApp/LogInViewController.m
@@ -0,0 +1,38 @@
+//
+//  LogInViewController.m
+//  PrivateDistributorApp
+//
+//  Created by VindicatoIOS on 11/20/13.
+//  Copyright (c) 2013 VindicatoIOS. All rights reserved.
+//
+
+#import "LogInViewController.h"
+
+@interface LogInViewController ()
+
+@end
+
+@implementation LogInViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
